bb979bc01678fe2f97359d98e2f3d794428025d1
Nishan9t <pandeynisha9t@gmail.com>
2023-09-01 15:58:26 +0530

    signup and login page frontend is done

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
 import Login from './components/Login';
 import Signup from './components/Signup';
-import ContextProvider from './Context/ContextProvider';
+import ContextProvider, { DataContext } from './Context/ContextProvider';
 import Main from './components/Main';
+import { useContext } from 'react';
 
 function App() {
+ 
   return (
     <ContextProvider>
        <BrowserRouter>
 
           <Routes>
+           
               <Route path='/login' element={<Login/>} />
 
               <Route path='/signup' element={<Signup/>} />
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,6 +2,44 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className="flex justify-center items-center h-screen bg-gray-400">
+    <form className="bg-white w-1/2  p-8 rounded shadow-md" >
+      <h2 className="text-2xl font-semibold mb-6">Sign In</h2>
+      <div className="mb-4">
+        <label className="block text-gray-600 font-semibold mb-2" htmlFor="email">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="w-full p-2 border rounded focus:outline-none focus:border-blue-400"
+        //   onChange={handleChange}
+        //   value={formData.email}
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-600 font-semibold mb-2" htmlFor="password">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          className="w-full p-2 border rounded focus:outline-none focus:border-blue-400"
+        //   onChange={handleChange}
+        //   value={formData.password}
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-blue-500 text-white font-semibold p-2 rounded hover:bg-blue-600"
+      >
+        Login
+      </button>
+    </form>
+  </div>
   )
 }
