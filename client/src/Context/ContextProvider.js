76c5b078658a80aaf6bca3d141e696d93c10a712
Nishan9t <pandeynisha9t@gmail.com>
2023-09-01 15:07:24 +0530

    context provider is completed

---
+++ client/src/Context/ContextProvider.js
@@ -0,0 +1,19 @@
+
+import {createContext, useState} from 'react';
+
+export const DataContext=createContext(null);
+
+const ContextProvider=({children})=>{
+    const [account,setAccount]=useState({username:'',name:''});
+
+    return(
+        <DataContext.Provider value={{
+            account,
+            setAccount
+        }}>
+            {children}
+        </DataContext.Provider>
+    )
+}
+
+export default ContextProvider;
