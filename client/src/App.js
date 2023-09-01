ad76f934d95b10d5fa532cf46545b9264bb1ead9
Nishan9t <pandeynisha9t@gmail.com>
2023-09-01 15:20:16 +0530

    routes are created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
+import Login from './components/Login';
+import Signup from './components/Signup';
+import ContextProvider from './Context/ContextProvider';
+import Main from './components/Main';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <ContextProvider>
+       <BrowserRouter>
+
+          <Routes>
+              <Route path='/login' element={<Login/>} />
+
+              <Route path='/signup' element={<Signup/>} />
+              <Route path='/' strict element={<Navigate replace to="/login" />} />
+          </Routes>
+        </BrowserRouter>
+    </ContextProvider>
   );
 }
 
