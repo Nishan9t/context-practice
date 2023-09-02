02408aea45b35ace4fc688983685ffad43f9af62
Nishan9t <pandeynisha9t@gmail.com>
2023-09-02 11:52:54 +0530

    db connected

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const DbConnection = require("./database/db");
 
 
 const app= express();
 
-
+//db connection
+DbConnection();
 
 app.listen(8000,()=>{
     console.log("server is running on port 8000")
